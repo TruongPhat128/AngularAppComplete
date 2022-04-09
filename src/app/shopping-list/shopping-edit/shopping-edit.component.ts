33da2b447eb28b85c4eaa42748c10bcd431f292b
TruongPhat128 <kemchua2927@gmail.com>
2022-04-09 17:19:06 +0700

    commit bt

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,28 @@
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { ShoppingListService } from '../shopping-list.service';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+  ingredient!: Ingredient;
+
+  constructor(private shoppingListService: ShoppingListService) { }
+  @ViewChild('nameInput') nameInput!: ElementRef;
+  @ViewChild('amountInput') amountInput!: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  @Output() onClear = new EventEmitter<void>();
+  ngOnInit(): void {
+  }
+
+  onAddItem(){
+    const ingredientName = this.nameInput.nativeElement.value;
+    const ingredientAmount = this.amountInput.nativeElement.value;
+    this.ingredient = new Ingredient(ingredientName, ingredientAmount);
+    this.ingredientAdded.emit(this.ingredient);
+    this.shoppingListService.addIngredient(this.ingredient);
+  }
+}
