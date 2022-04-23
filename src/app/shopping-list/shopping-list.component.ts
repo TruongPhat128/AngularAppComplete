f0ac9ae5e75a7b531dcafe5028c10e9c99ee61ed
TruongPhat128 <kemchua2927@gmail.com>
2022-04-23 16:00:19 +0700

    buoi 6

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,18 +1,23 @@
-
-import { Ingredient } from './../shared/ingredient.model'
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
-
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [];
+  ingredients!: Ingredient[];
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
-    this.ingredients = this.shoppingListService.getIngredient();
+    this.ingredients = this.shoppingListService.getIngredients();
   }
+
+    onEditItem(index: number | undefined){
+      this.shoppingListService.startedEditing.emit(index)
+    }
+  // onIngredientAdd(ingredient: Ingredient){
+  //   this.ingredients.push(ingredient);
+  // }
 }
